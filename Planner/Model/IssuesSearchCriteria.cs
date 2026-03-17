fe5df99a9f64c438155e331f0535c86d5b5bd8f0
Alberto Viezzi <alberto.viezzi@euris.it>
2026-03-17 20:15:05 +0100

    Refactor FiltersControl: add status filter support, update layout, and improve project selection logic

--- Planner/Model/IssuesSearchCriteria.cs
+++ Planner/Model/IssuesSearchCriteria.cs
@@ -7,6 +7,7 @@ public record IssuesSearchCriteria
     public string ProjectKey { get; init; } = string.Empty;
     
     public IFilterSelection<TypeModel> Types { get; init; } = FilterSelection<TypeModel>.Empty;
+    public IFilterSelection<StatusModel> Statuses { get; init; } = FilterSelection<StatusModel>.Empty;
     public IFilterSelection<ComponentModel> Components { get; init; } = FilterSelection<ComponentModel>.Empty;
     public IFilterSelection<LabelModel> Labels { get; init; } = FilterSelection<LabelModel>.Empty;
     
